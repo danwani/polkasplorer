d0d674800034589401155b3234cb5245272c55f1
Dan Wani <danwani@gmail.com>
2021-04-27 20:58:17 +1000

    Reformatted code - 2 spaces, not 4!

--- scripts/server.js
+++ scripts/server.js
@@ -4,14 +4,14 @@ const basicAuth = require('express-basic-auth')
 const app = express();
 
 app.use(basicAuth({
-    users: { 'admin': 'acalaforthewin' },
-    challenge: true,
+  users: {'admin': 'acalaforthewin'},
+  challenge: true,
 }))
 
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(3000, () => console.log('Server started on port 3000'));
--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,90 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-import {Form, FormGroup, FormLabel, FormControl, Button} from 'react-bootstrap';
+import {Button, Form, FormControl, FormGroup, FormLabel, InputGroup} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component<
-    {},
-    {
-        startBlock: string;
-        endBlock: string;
-        endPoint: string;
+class App extends React.Component<{},
+  {
+    startBlock: string;
+    endBlock: string;
+    endPoint: string;
+  }> {
+  state = {
+    startBlock: "",
+    endBlock: "",
+    endPoint: "rpc.polkadot.io"
+  };
+
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    alert(`Querying for... ${this.state.startBlock} to ${this.state.endBlock} on ${this.state.endPoint}`);
+    e.preventDefault();
+  }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    switch (e.currentTarget.name) {
+      case 'startBlock':
+        this.setState({startBlock: e.currentTarget.value});
+        break;
+      case 'endBlock':
+        this.setState({endBlock: e.currentTarget.value});
+        break;
+      case 'endPoint':
+        this.setState({endPoint: e.currentTarget.value});
+        break;
     }
-    > {
-    state = {
-        startBlock: "",
-        endBlock: "",
-        endPoint: "wss://rpc.polkadot.io"
-    };
+  };
 
-    handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-        alert(`Querying for... ${this.state.startBlock} to ${this.state.endBlock} on ${this.state.endPoint}`);
-        e.preventDefault();
-    }
-
-    handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        switch (e.currentTarget.name) {
-            case 'startBlock':
-                this.setState({startBlock: e.currentTarget.value});
-                break;
-            case 'endBlock':
-                this.setState({endBlock: e.currentTarget.value});
-                break;
-            case 'endPoint':
-                this.setState({endPoint: e.currentTarget.value});
-                break;
-        }
-    };
-
-    render() {
-        return (
-                <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>
-          Polkasplorer!
-        </h2>
-      </header>
-      <section className="block-details">
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>
+            Polkasplorer!
+          </h2>
+        </header>
+        <section className="block-details">
           <p>Please enter the block details you'd like to query</p>
           <Form onSubmit={this.handleSubmit}>
-              <FormGroup>
-                  <FormLabel>
-                      Start block:
-                  </FormLabel>
-                  <FormControl type="number" name="startBlock" value={this.state.startBlock} onChange={this.handleChange} />
-              </FormGroup>
-              <FormGroup>
-                  <FormLabel>
-                      End block:
-                  </FormLabel>
-                  <FormControl type="number" name="endBlock" value={this.state.endBlock} onChange={this.handleChange} />
-              </FormGroup>
-              <FormGroup>
-                  <FormLabel>
-                      Endpoint:
-                  </FormLabel>
-                  <FormControl type="text" name="endPoint" value={this.state.endPoint} onChange={this.handleChange} />
-              </FormGroup>
-              <Button variant="primary" type="submit">
-                  Submit
-              </Button>
+            <FormGroup>
+              <FormLabel>Start block:</FormLabel>
+              <InputGroup hasValidation>
+                <FormControl type="number" name="startBlock" value={this.state.startBlock} onChange={this.handleChange}
+                             required/>
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid start block
+                </Form.Control.Feedback>
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>End block:</FormLabel>
+              <InputGroup hasValidation>
+                <FormControl type="number" name="endBlock" value={this.state.endBlock} onChange={this.handleChange}
+                             required/>
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid end block
+                </Form.Control.Feedback>
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Endpoint:</FormLabel>
+              <InputGroup hasValidation>
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroupPrepend">wss://</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl type="text" name="endPoint" value={this.state.endPoint} onChange={this.handleChange}
+                             aria-describedby="inputGroupPrepend"/>
+              </InputGroup>
+            </FormGroup>
+            <Button variant="primary" type="submit">
+              Scan
+            </Button>
           </Form>
-      </section>
-        <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
-
-        <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
-
-        <script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"></script>
-
-        <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-            crossOrigin="anonymous"
-        />
-    </div>
-        );
-    }
+        </section>
+      </div>
+    );
+  }
 }
 
 // function App() {
